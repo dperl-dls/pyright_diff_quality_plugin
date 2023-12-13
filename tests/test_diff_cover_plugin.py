e90530e24ee462b20ebde8444427f8a6c8dff9f4
David Perl <david.perl@diamond.ac.uk>
2023-12-13 17:52:08 +0000

    remove local test

--- tests/test_diff_cover_plugin.py
+++ tests/test_diff_cover_plugin.py
@@ -4,7 +4,4 @@ from diff_cover.diff_quality_tool import main
 def test_plugin_can_load():
     main(["diff-quality","--violations","pyright"])
 
-def test_run_on_hyperion():
-    import os
-    os.chdir("../hyperion/hyperion/")
-    main(["diff-quality","--violations","pyright"])
+
