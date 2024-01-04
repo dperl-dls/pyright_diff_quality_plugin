b71cc5ae9d801105b264645bade8e9d4fa3fce0d
Dominic Oram <dominic.oram@diamond.ac.uk>
2024-01-04 00:20:27 +0000

    (#5) Fix linter

--- tests/test_diff_cover_plugin.py
+++ tests/test_diff_cover_plugin.py
@@ -1,4 +1,3 @@
-import json
 import os
 from unittest.mock import patch
 
@@ -10,6 +9,7 @@ from pyright_diff_quality_plugin.pyright_plugin import PyrightViolationReporter
 def test_plugin_can_load():
     main(["diff-quality", "--violations", "pyright"])
 
+
 @patch("pyright_diff_quality_plugin.pyright_plugin.exists")
 def test_given_file_does_not_exist_then_return_no_violations(mock_exists):
     mock_exists.return_value = False
@@ -18,10 +18,13 @@ def test_given_file_does_not_exist_then_return_no_violations(mock_exists):
     violations = reporter.violations("")
     assert violations == []
 
+
 @patch("pyright_diff_quality_plugin.pyright_plugin.exists")
 @patch("pyright_diff_quality_plugin.pyright_plugin.subprocess.run")
-def test_given_file_does_exist_and_test_json_used_then_return_expected_violations(mock_subprocess, mock_exists):
-    with open(os.path.join("tests", "test_data","test_json.json")) as f:
+def test_given_file_does_exist_and_test_json_used_then_return_expected_violations(
+    mock_subprocess, mock_exists
+):
+    with open(os.path.join("tests", "test_data", "test_json.json")) as f:
         mock_subprocess.return_value.stdout = f.read()
     mock_exists.return_value = True
 
